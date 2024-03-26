3d071bd7245b210a02c25f4780f92142d1fbe74b
njzw <nigeljaure@icloud.com>
2024-03-26 13:45:45 +0200

    contipay checksum generator

--- src/Core/Checksums/ContipayChecksum.php
+++ src/Core/Checksums/ContipayChecksum.php
@@ -6,21 +6,9 @@ use Exception;
 
 class ContipayChecksum
 {
-    /**
-     * Generate checksum using OpenSSL signing.
-     *
-     * @param mixed $data The data to generate the checksum for.
-     * @param bool $encode Whether to encode the checksum in base64.
-     * @param string $privateKeyPath The path to the file containing the private key.
-     * @param int $algo The algorithm to use for signing (default is SHA256).
-     * @return string The generated checksum.
-     */
-    function generateChecksum(mixed $data, bool $encode = true, string $privateKeyPath = "", int $algo = OPENSSL_ALGO_SHA256): string
+    function generateChecksum($data, bool $encode = true, $privateKey = "", int $algo = OPENSSL_ALGO_SHA256): string
     {
-
-        if (!empty ($privateKeyPath) && file_exists($privateKeyPath)) {
-
-            $privateKey = file_get_contents($privateKeyPath);
+        if (!empty ($privateKey)) {
 
             if (openssl_sign($data, $signature, $privateKey, $algo)) {
 
